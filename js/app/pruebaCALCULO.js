bde0a388233b9e7ccc441c477026aa8b5a290ebe
Ricardo <ricpedraza001@gmail.com>
2023-05-12 19:35:35 -0300

    Se logro realizar la operación de las ultimas dos columnas (Total de presentes y ausentes)

--- js/app/pruebaCALCULO.js
+++ js/app/pruebaCALCULO.js
@@ -1,12 +1,42 @@
 const btnCalcular = document.querySelector('#btnCalcular');
 const tBody = document.querySelector("[data-tr-filas]");
+let alumnos = document.querySelector("#input_alumnos").value;
+alumnos = parseInt(alumnos);
 
 btnCalcular.addEventListener("click",()=>{
     //Sección de llamadas:
-    filaTD(0);
+    construirTitulos_UltimasColumnas();
+
+    for (let i = 0; i < alumnos; i++) {        
+        filaTD(i);
+    }
 
 });
 
+/////////////////// ULTIMAS (2) COLUMNAS ////////////////////////
+function construirTitulos_UltimasColumnas(){
+    const tHead_TH = document.querySelector("[data-tr-dias]");
+    
+    const thPresentes = document.createElement('th');
+    thPresentes.innerHTML = "Presentes";
+    tHead_TH.appendChild(thPresentes);
+
+    const thAusentes = document.createElement('th');
+    thAusentes.innerHTML = "Ausentes";
+    tHead_TH.appendChild(thAusentes);
+}
+
+function construirUltimasColumnas(p,a,rowIndice){
+    const filaTR = tBody.rows[rowIndice];
+    const elementTH_Presentes = document.createElement('th');
+    elementTH_Presentes.innerHTML = p;
+    filaTR.appendChild(elementTH_Presentes);
+
+    const elementTH_Ausentes = document.createElement('th');
+    elementTH_Ausentes.innerHTML = a;
+    filaTR.appendChild(elementTH_Ausentes);
+    
+}
 
 function filaTD(rowIndice){
     var array_P=[], array_A=[];
@@ -31,33 +61,7 @@ function filaTD(rowIndice){
     let presentes = array_P.length; //por fila
     let ausentes = array_A.length; //por fila
     console.log(presentes,ausentes)
-    construirTitulos_UltimasColumnas();
     construirUltimasColumnas(presentes,ausentes,rowIndice);
 }
 
-function construirTitulos_UltimasColumnas(){
-    const tHead_TH = document.querySelector("[data-tr-dias]");
-    
-    const thPresentes = document.createElement('th');
-    thPresentes.innerHTML = "Presentes";
-    tHead_TH.appendChild(thPresentes);
-
-    const thAusentes = document.createElement('th');
-    thAusentes.innerHTML = "Ausentes";
-    tHead_TH.appendChild(thAusentes);
-
-
-}
-
-//construir las dos últimas columnas:
-function construirUltimasColumnas(p,a,rowIndice){
-    const filaTR = tBody.rows[rowIndice];
-    const elementTH_Presentes = document.createElement('th');
-    elementTH_Presentes.innerHTML = p;
-    filaTR.appendChild(elementTH_Presentes);
-
-    const elementTH_Ausentes = document.createElement('th');
-    elementTH_Ausentes.innerHTML = a;
-    filaTR.appendChild(elementTH_Ausentes);
-    
-}
+/////////////////// ULTIMAS (2) FILAS ////////////////////////
